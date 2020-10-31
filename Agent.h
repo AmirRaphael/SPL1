a3f07afb7a8e8d0f343f16e978911dbb06f39490
Raphael-cohen-96 <73221651+Raphael-cohen-96@users.noreply.github.com>
2020-10-31 14:19:40 +0200

    Update Agent.h
    Virus & CT are no longer pure virtual classes (after forum update)

--- Agent.h
+++ Agent.h
@@ -8,7 +8,6 @@ class Session;
 class Agent{
 public:
     Agent(Session& session);
-
     virtual void act()=0;
 private:
     Session& session;
@@ -17,15 +16,13 @@ private:
 class ContactTracer: public Agent{
 public:
     ContactTracer(Session& session);
-
-    virtual void act()=0;
+    virtual void act();
 };
 //============Virus==============
 class Virus: public Agent{
 public:
     Virus(int nodeInd, Session& session);
-
-    virtual void act()=0;
+    virtual void act();
 private:
     const int nodeInd;
 };
